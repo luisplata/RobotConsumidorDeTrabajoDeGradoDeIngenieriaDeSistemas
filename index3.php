32c70bb498045897bd661eff5b9197f31ec26d7f
luis plata <www.luisplata@gmail.com>
2017-05-02 02:19:08 -0500

    Se completa el desarrollo con el cambio de estados en distintas faces, para poder volver a cargar sin tener que cargar la pagina

--- index3.php
+++ index3.php
@@ -11,24 +11,9 @@
 		<script type="text/javascript" src="./lib/kiwi.min.js"></script>
 		<script src="./lib/sweetalert.min.js"></script>
 		
-		<script>		
-			/*
-			Aqui se realiza las peticiones hacia el servidor donde debe responder que hay una solucion al laberinto
-			*/
-			
-			document.addEventListener("DOMContentLoaded", function(){
-				game = new Kiwi.Game('', 'Proyecto de grado', carga, gameOptions);
-				//game.states.addState( carga );
-				game.states.addState( lectura );
-				game.states.addState( play );
-				game.states.addState( fin );
-			});
-			//Las opciones del simulador
-			
-		</script>
-		
 		<script>
-			
+		
+			var state = new Kiwi.State('Play');
 			
 			var game;
 			var mi_imagen;
@@ -40,199 +25,102 @@
 			var instrucciones ;
 			var tamanioDePantalla;
 			var posicionAnterior = {x:0,y:0};
-			var gameOptions = {
-				renderer: Kiwi.RENDERER_WEBGL,
-				width: 615,
-				height: 615
-			};
 
-			var carga = new Kiwi.State('carga');
-			carga.preload = function(){
-				//todo code...
-				//solo cargo los datos para el cambio de estado
-				console.log("Busqueda de recursos");
-				document.title = "cargando los datos";
-				
-			}
-			carga.create = function(){
-				//todo code...
-				//cambio de estado
-				ajaxGet("http://localhost/imagenes/core/BuscarRespuesta.php");
-				game.states.switchState( "lectura" );
-			}
-			
-			var lectura = new Kiwi.State('lectura');
-			lectura.preload = function(){
-				//todo code...
-				console.log("Leyendo los datos");
-				document.title = "Leyendo los datos";
-			}
-			lectura.create = function(){
-				//todo code...
-				//cambio de estado
-				//averiguamos si hay respuesta
-				if(instrucciones.hayRespuesta){
-					//leemos los datos
-						
-					mi_imagen = new Image();
-					robot = new Image();
-					
-					mi_imagen.src = "http://localhost/imagenes/Imagenes/Laberintos/"+instrucciones.imagen;
-					robot.src= "./images/robot.png";
-					
-					//si no carga alguno reiniciamos hasta que cargue
-					var error = false;
-					mi_imagen.height == 0 ? error = true:console.log("cargo el laberinto");
-					robot.height == 0 ? error = true:console.log("cargo el robot");
-					
-					//obtenemos el tamaño de la imagen del robot
-					robot.mitad = ((robot.height*escalaRobot)-robot.height)/2;
-					
-					//cambio de estado
-					if(error){
-						game.states.switchState( "carga" );
-					}else{
-						game.states.switchState( "Play" );	
-					}
-				}else{
-					game.states.switchState( "carga" );
-				}	
-			}
-			
-			var fin = new Kiwi.State('fin');
-			fin.preload = function(){
-				//todo code...
-				console.log("fin del juego");
-				document.title = "Fin del Juego";
-			}
-			fin.create = function(){
-				//todo code...
-				//cambio de estado
-				
-				swal({
-				  title: "Fin del laberinto!",
-				  text: "El robot ha llegado a su destino, desea cargar de nuevo el laberinto?",
-				  type: "success",
-				  showCancelButton: true,
-				  confirmButtonColor: "#5c6bc0",
-				  confirmButtonText: "Recargar",
-				  cancelButtonColor: "#ef9a9a",
-				  cancelButtonText: "Finalizar",
-				  closeOnConfirm: true,
-				  closeOnCancel: false
-				},
-				function(isConfirm){
-				  if (isConfirm) {
-					location.reload(true);
-				  } else {
-					swal("Gracias", "Por utilizar nuestro simulador", "success");
-				  }
-				});
-			}
-			
-			var play = new Kiwi.State('Play');
-			play.preload = function () {
+			state.preload = function () {
 				//cargamos el fondo
-				document.title = "Jugando! :v";
+				
 				this.addImage('tablero', mi_imagen.src);
 				this.addImage('robot',robot.src);
 			};
 
-			play.create = function () {
+			state.create = function () {
 
+			//variables
+				//this.contador = 0;
 				//objetos
 				this.tablero = new Kiwi.GameObjects.Sprite(this, this.textures.tablero, 0, 0);
+				
 				this.robot = new Kiwi.GameObjects.Sprite(this, this.textures.robot,0,0);
+				
+				//Creamos el texto de fin de juego
+				this.fin = new Kiwi.HUD.Widget.TextField( this.game, 'FIN DEL LABERINTO', 50, 50 );
+				this.game.huds.defaultHUD.addWidget(this.fin);
+				this.fin.x = -100;
+				this.fin.y = -100;
+				this.fin.style.color = '#5F5F5F';
 				
 				//creamos la altura en X y Y para ponerlos en 0 al iniciar
 				this.robot.x2 = this.robot.y2 = 0;
-	
-				//agregandolos a la escena
+				
+				//console.log(robot.mitad);
+
 				this.addChild(this.tablero);
 				this.addChild(this.robot);
 				
+				//escalada del tablero
+				//this.tablero.scaleY=0.5;
+				//this.tablero.scaleX=0.5;
+				//posicionamiento del tablero
+				//this.tablero.x = -(cuarto);
+				//this.tablero.y = -(cuarto);
+				var escalada = escalaRobot;
 				//escalada del robot
-				this.robot.scaleY = this.robot.scaleX = escalaRobot;
+				this.robot.scaleY = this.robot.scaleX = escalada;
 				//posicionamiento del robot
 				tamanioDePantalla = (this.game.stage.width/8);
 				this.robot.x = (robot.mitad);
 				this.robot.y =(robot.mitad);
+				//console.log(this.robot.x);
 				
 				//ahora calculamos el lugar de partida, apartir de estar en el 0,0
 				if(instrucciones.puntoEntrada >= 0 && instrucciones.puntoEntrada <= 7){
 					this.robot.x += instrucciones.puntoEntrada*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*0;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 8 && instrucciones.puntoEntrada <= 15){
 					this.robot.x += (instrucciones.puntoEntrada - (8*1))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*1;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 16 && instrucciones.puntoEntrada <= 23){
 					this.robot.x += (instrucciones.puntoEntrada - (8*2))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*2;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 24 && instrucciones.puntoEntrada <= 31){
 					this.robot.x += (instrucciones.puntoEntrada - (8*3))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*3;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 32 && instrucciones.puntoEntrada <= 39){
 					this.robot.x += (instrucciones.puntoEntrada - (8*4))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*4;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 40 && instrucciones.puntoEntrada <= 47){
 					this.robot.x += (instrucciones.puntoEntrada - (8*5))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*5;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 48 && instrucciones.puntoEntrada <= 55){
 					this.robot.x += (instrucciones.puntoEntrada - (8*6))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*6;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}else if(instrucciones.puntoEntrada >= 56 && instrucciones.puntoEntrada <= 63){
 					this.robot.x += (instrucciones.puntoEntrada - (8*7))*tamanioDePantalla;
 					this.robot.y += tamanioDePantalla*7;
+					//console.log("X: "+this.robot.x+" Y: "+this.robot.y);
 					
 				}
 				
 				posicionAnterior.x = this.robot.x;
 				posicionAnterior.y = this.robot.y;
+				//this.robot.x = (robot.mitad)+672;
+				//this.robot.y =(robot.mitad)+;//96 es el tamaño de cada cuadro
 				
 			};
-			
-			
-			play.update = function () {
-				//se ejecutara toda la secuencia del robot andante
-				//para despues realizar las rotaciones del robot para que se vea realiza
-				
-				//se realiza el movimiento
-				switch(instrucciones.secuencia[indice]){
-					case 0:
-						//adelante
-						adelante(this.robot.x,this.robot.y,this.robot);
-						//console.log(instrucciones.secuencia[indice]);
-						break;
-					
-					case 2:
-						//giro izquierda
-						giroIzquierda(this.robot);
-						//console.log(instrucciones.secuencia[indice]);
-						break;
-					
-					case 1:
-						//giro derecha
-						giroDerecha(this.robot);
-						//console.log(instrucciones.secuencia[indice]);
-						break;
-				}
-				
-				
-				if(indice == instrucciones.secuencia.length){
-					//ejecutamos la secuencia de fin del juego
-					game.states.switchState( "fin" );
-				}
-				
-			};
-
-			
 			var indice = 0;//indicar por donde vamos
 			var haciaDondeMiraElRobot = 2;//0=arriba;1=derecha;2=abajo;3=izquierda
 			var velocidad = 1;
@@ -249,6 +137,7 @@
 			function abajo(robot){
 				robot.y += velocidad;
 			}
+			
 			function adelante(x,y,robot){
 				
 				
@@ -290,7 +179,8 @@
 						break;
 				}
 				
-			}			
+			}
+			
 			function giroDerecha(robot){
 				if(haciaDondeMiraElRobot == 3){
 					haciaDondeMiraElRobot =0;
@@ -311,23 +201,142 @@
 				
 				robot.rotation -= Math.PI * 0.5
 				indice++;
-			}		
-			function ajaxGet(url) {
+			}
+			
+			state.update = function () {
+				//se ejecutara toda la secuencia del robot andante
+				//para despues realizar las rotaciones del robot para que se vea realiza
+				//console.log(instrucciones.secuencia[indice]);
+				
+				//movimiento del robot va a ser de 5 px
+				//console.log(instrucciones.secuencia[indice]);
+				//se realiza el movimiento
+				switch(instrucciones.secuencia[indice]){
+					case 0:
+						//adelante
+						adelante(this.robot.x,this.robot.y,this.robot);
+						//console.log(instrucciones.secuencia[indice]);
+						break;
+					
+					case 2:
+						//giro izquierda
+						giroIzquierda(this.robot);
+						//console.log(instrucciones.secuencia[indice]);
+						break;
+					
+					case 1:
+						//giro derecha
+						giroDerecha(this.robot);
+						//console.log(instrucciones.secuencia[indice]);
+						break;
+				}
+				
+				
+				if(indice == instrucciones.secuencia.length){
+					//ejecutamos la secuencia de fin del juego
+					//console.log("Fin del juego");
+					//this.fin.style.fontSize = '40px';
+					//this.fin.x = this.game.stage.width/4;
+					//this.fin.y = this.game.stage.width/2;
+					sweetAlert("Fin del Laberinto", "Para Cargar de nuevo el laberinto, Actualize la pagina", "success");
+					//this.game.states.switchState( "OutsideState" );
+					//location.reload(true);
+					//ajaxGet("http://pensante.nabu.com.co/core/BuscarRespuesta.php",comenzar,"http://pensante.nabu.com.co/");
+					
+				}
+				
+			};
+
+
+
+		</script>
+		
+		<script>		
+			/*
+			Aqui se realiza las peticiones hacia el servidor donde debe responder que hay una solucion al laberinto
+			*/
+			var gameOptions = {
+				renderer: Kiwi.RENDERER_WEBGL,
+				width: 512,
+				height: 512
+			};
+			document.addEventListener("DOMContentLoaded", function(){
+			  
+				//console.log("Comenzo a preguntar");
+				//en 10 sec
+				//setTimeout(comenzar,200);
+				//http://pensante.nabu.com.co/core/BuscarRespuesta.php
+				//ajaxGet("./seudoPensante.php",comenzar);
+				ajaxGet("http://pensante.nabu.com.co/core/BuscarRespuesta.php",comenzar,"http://pensante.nabu.com.co/");
+			});
+			//Las opciones del simulador
+			
+			function ajaxGet(url, funcion, url_servidor) {
 			  var xhttp = new XMLHttpRequest();
 			  xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
 					//console.log(this.responseText);
 					json = this.responseText;
 					instrucciones = JSON.parse(json);
+					if(!instrucciones.hayRespuesta){
+						location.reload(true);
+					}else{
+					    funcion(instrucciones);    
+					}
+					
+				}else{
+					
 				}
 			  };
-			  xhttp.open("GET", url, false);
+			  xhttp.open("GET", url, true);
 			  xhttp.send();
 			}
 			
-
+			function comenzar(instrucciones){
+				
+				//obtenemos el json
+				//instrucciones = '{"imagen":"./images/Tablero.jpg","puntoEntrada":0,"secuencia":[0,0,1,0,0,0,2,0,1,0,2,0,1,0,2,0,0,1,0,0,1,0]}';
+				
+				
+				//console.log("http://pensante.nabu.com.co/Imagenes/Laberintos/"+instrucciones.imagen);
+				
+				
+				mi_imagen = new Image();
+				// .crossOrigin = "anonymous";
+				//mi_imagen.crossOrigin = "Anonymous";
+				robot = new Image();
+				
+				mi_imagen.src = "http://pensante.nabu.com.co/Imagenes/Laberintos/"+instrucciones.imagen;
+				console.log(mi_imagen.height);
+				robot.src= "./images/robot.png";
+				
+				//console.log(mi_imagen.height);
+				//console.log(robot.height);
+				
+				//si no carga alguno reiniciamos hasta que cargue
+				if(mi_imagen.height == 0){
+				    //sweetAlert("Fin del Laberinto", "Para Cargar de nuevo el laberinto Actualize la pagina", "warning");
+				    location.reload(true);
+				    //ajaxGet("http://pensante.nabu.com.co/core/BuscarRespuesta.php",comenzar,"http://pensante.nabu.com.co/");
+				}
+				 
+				robot.height == 0 ? location.reload(true):console.log("cargo el robot");
+				
+				//obtenemos el tamaño de la imagen a partir de su 50%;
+				mitad = mi_imagen.height/2;
+				cuarto = mitad/2;
+				//obtenemos el tamaño de la imagen del robot
+				robot.mitad = ((robot.height*escalaRobot)-robot.height)/2;
+				
+				gameOptions.width=mi_imagen.height;
+				gameOptions.height=mi_imagen.height;
+				
+				
+				//console.log(mi_imagen.height);
+				
+				game = new Kiwi.Game('', 'Proyecto de grado', state, gameOptions);
+			}
 		</script>
-		
 	
     </body>
 </html>
